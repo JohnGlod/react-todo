379e9f5a0017f73872fa12f7d295f3275b218b2d
Evgeniy Dolgopolov <EvgeniiDolgopolov@outlook.com>
2022-08-12 22:40:35 +0500

    рефактор компонента

---
+++ src/components/TodoList/TodoList.props.ts
@@ -0,0 +1,5 @@
+import { ITodo } from '../../model/ITodo';
+
+export interface TodoListProps {
+  todos: ITodo[] | [];
+}
--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,29 +1,11 @@
-import { useContext } from 'react';
-import { TodoContext } from '../../store/store';
-
-import { ITodo } from '../../model/ITodo';
-import { EActions } from '../../model/EActions';
-
 import { TodoItem } from '../TodoItem';
+
+import { TodoListProps } from './TodoList.props';
 
 import styles from './TodoList.module.scss';
 
-export const TodoList = () => {
-  const { state, dispatch } = useContext(TodoContext);
-  return (
-    <ul className={styles.list}>
-      {!!state?.todoList &&
-        state.todoList?.map((todo: ITodo, id) => {
-          return (
-            <TodoItem
-              key={id}
-              todo={todo}
-              onClick={() =>
-                dispatch && dispatch({ type: EActions.ACTIVE, payload: todo })
-              }
-            />
-          );
-        })}
-    </ul>
-  );
-};
+export const TodoList = ({ todos }: TodoListProps) => (
+  <ul className={styles.list}>
+    {!!todos && todos?.map((todo) => <TodoItem key={todo.id} todo={todo} />)}
+  </ul>
+);
